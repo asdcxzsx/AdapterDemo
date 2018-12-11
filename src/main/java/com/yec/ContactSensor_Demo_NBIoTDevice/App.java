176948df2996c15f1b5ba43ef3ccf9c5f0ba68c0
汤磊 <252945076@qq.com>
2018-12-11 13:54:08 +0800

    Test Input

--- src/main/java/com/yec/ContactSensor_Demo_NBIoTDevice/App.java
+++ src/main/java/com/yec/ContactSensor_Demo_NBIoTDevice/App.java
@@ -6,8 +6,8 @@ import javax.swing.JLabel;
 public class App {
 
 	/**{
-     * 创建并显示GUI。出于线程安全的考虑，
-     * 这个方法在事件调用线程中调用。
+     * 创建并显示GUI。出于线程安全的考虑，  
+     * 这个方法在事件调用线程中调用。  Unit Test
      */
     private static void createAndShowGUI() {
         // 确保一个漂亮的外观风格
@@ -22,7 +22,6 @@ public class App {
         // 添加 "Hello World" 标签
         JLabel label = new JLabel("Hello World");
         frame.getContentPane().add(label);
-
         // 显示窗口
         //frame.pack();
         frame.setVisible(true);
